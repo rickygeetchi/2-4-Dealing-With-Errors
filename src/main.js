eb0612a7ee5d7b1d282886622fc0730e7e0cd726
rickymsmith <35548840+rickymsmith@users.noreply.github.com>
2022-03-31 18:55:55 -0500

    updating code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-let canDrive = true;
+let canDrive = false;
 
-if (canDrive = true) {
-    console.log("You can drive")
+if (canDrive == true) {
+    console.log("You can drive");
 } else {
-    console.log("You can not drive")
+    console.log("You can not drive");
 }
