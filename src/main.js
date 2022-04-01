9f1d8a9453de5ededde67258f6f601da90d9bc3b
rickymsmith <35548840+rickymsmith@users.noreply.github.com>
2022-03-31 20:29:00 -0500

    adding three variables + added prompt-sync package

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-let canDrive = false;
 
-if (canDrive) {
+const hasLicense = true;
+const myAge = 17;
+const drivingAge = 18;
+
+if (hasLicense && myAge >= drivingAge) {
     console.log("You can drive");
 } else {
     console.log("You can not drive");
 }
+
+
